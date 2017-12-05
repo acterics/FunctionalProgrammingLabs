f04cd306a5b6fc5e5cc91368c4acdae3b6286cb4
Oleg Lipskiy <lolego1601@gmail.com>
2017-12-05 07:57:36 +0200

    pasre env file

--- lab1/app/Main.hs
+++ lab1/app/Main.hs
@@ -1,6 +1,29 @@
 module Main where
 
 import Person
+import Data.List.Split
+import Data.List
+import Data.Ord
+
+
+file_value_list:: String -> [String]
+file_value_list file = splitOneOf  "=\n" file
+
+filter_list :: String -> String
+filter_list = show . map snd . filter get_even_index . zip [0..] . file_value_list where
+    get_even_index = odd . fst 
+-- filter_even = map (snd) filter_list where
+--     filter_list = filter (\(i, _) -> even i) . zip [0..]
+    
+
+
 
 main :: IO ()
-main = helloworld
+main = do
+    string <- readFile ".env"
+    
+    putStrLn (filter_list string)
+
+    -- conn <- connectPostgreSQL "host=localhost dbname=sport_univ_db user=postgres password=root"
+
+    
--- lab1/src/Person.hs
+++ lab1/src/Person.hs
@@ -9,7 +9,5 @@ type FirstName = String
 type LastName = String
 data Position = Student | Teacher deriving (Enum)
 
-data Person = Person {id :: Id, firstName :: FirstName, lastName :: LastName, position :: Position}
-
-
+data Person = Person { id :: Id, firstName :: FirstName, lastName :: LastName, position :: Position }
 helloworld = putStrLn "Hello World"
