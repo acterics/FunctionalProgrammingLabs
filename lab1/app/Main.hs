c6a57918f9ca8c42a504af41a595718201f4a7fd
Oleg Lipskiy <lolego1601@gmail.com>
2017-12-05 09:39:12 +0200

    add parse env module

--- lab1/app/Main.hs
+++ lab1/app/Main.hs
@@ -1,28 +1,14 @@
 module Main where
 
 import Person
-import Data.List.Split
-import Data.List
-import Data.Ord
-
-
-file_value_list:: String -> [String]
-file_value_list file = splitOneOf  "=\n" file
-
-filter_list :: String -> String
-filter_list = show . map snd . filter get_even_index . zip [0..] . file_value_list where
-    get_even_index = odd . fst 
--- filter_even = map (snd) filter_list where
---     filter_list = filter (\(i, _) -> even i) . zip [0..]
-    
-
+import ParseEnv
 
 
 main :: IO ()
 main = do
     string <- readFile ".env"
-    
-    putStrLn (filter_list string)
+    putStrLn (get_env_db_connection string)
+    -- conn <- connectPostgreSQL (filter_list  string)
 
     -- conn <- connectPostgreSQL "host=localhost dbname=sport_univ_db user=postgres password=root"
 
---
+++ lab1/src/ParseEnv.hs
@@ -0,0 +1,26 @@
+
+
+module ParseEnv where 
+
+import Data.List.Split
+
+
+file_value_list:: String -> [String]
+file_value_list file = splitOneOf  "=\n" file
+
+concat_connection :: String -> [Char] -> Integer -> String
+concat_connection conn s 0 = conn++" password="++s++" "
+concat_connection conn s 1 = conn++" user="++s++" "
+concat_connection conn s 2 = conn++" dbname="++s
+
+concat_connection_base :: String -> (Integer, [Char]) -> String
+concat_connection_base conn c = concat_connection conn (snd c) (fst c)
+
+
+get_connection_string :: [(Integer, [Char])] -> String
+get_connection_string parsed_env = foldl concat_connection_base "host=localhost" parsed_env
+
+
+get_env_db_connection :: String -> String
+get_env_db_connection = get_connection_string . zip [0..] . map snd . filter get_even_index . zip [0..] . file_value_list where
+    get_even_index = odd . fst 
