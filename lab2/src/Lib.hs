8ca1a7685a8eaceec728cb1926b91a8eed6e31d6
Oleg Lipskiy <lolego1601@gmail.com>
2017-12-10 17:29:26 +0200

    working on pollard algorithm

--- lab2/src/Lib.hs
+++ lab2/src/Lib.hs
@@ -1,6 +1,43 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib(
+    pollard_rho
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- func :: Integer -> Integer -> Integer
+-- func x n = mod ( x * x - 1) n
+
+-- pollardStep :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+-- pollardStep i k n x y
+--         | d /= 1 && d /= n = d
+--         | i == k = pollardStep (i+1) (2*k) n x1 x1
+--         | otherwise = pollardStep (i+1) k n x1 y where
+--              d = gcd n $ abs $ y - x
+--              x1 = func x n
+
+-- pollard_rho :: Integer -> Integer
+-- pollard_rho n = pollardStep 1 2 n 2 2
+
+
+
+pollard_rho a b p = pollard_step_base a b p 0 0 0 0 1 1 1
+
+new_step_value :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> [Integer]
+new_step_value a b p x ai bi  
+    | (mod x 3) == 0 = map f [x^2, ai * 2, bi * 2]  
+    | (mod x 3) == 1 = map f [x * a, ai + 1, bi]
+    | (mod x 3) == 2 = map f [x * b, ai, bi + 1] where
+        f x' = mod x' p
+    
+
+pollard_step_base :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer
+pollard_step_base a b p ai a2i bi b2i xi x2i i = 
+    if xi == x2i then 
+        if bdif /= 0 then mod (quot adif bdif) p  
+        else -1
+    else new_step (i + 1) ival i2val where
+        adif = a2i - ai
+        bdif = bi - b2i
+        new_step i' [xi', ai', bi'] [x2i', a2i', b2i'] = pollard_step_base a b p ai' a2i' bi' b2i' xi' x2i' i'
+        ival = new_step_value a b p xi ai bi
+        i2val = new_step_value a b p x2i a2i b2i
+
+    
--- lab2/test/Spec.hs
+++ lab2/test/Spec.hs
@@ -1,2 +1,24 @@
+
+import Test.Hspec
+import Lib
+
+simple_method :: Integer -> Integer -> Integer -> Integer
+simple_method a b p = is_solved 0 where 
+    is_solved x = do
+        if mod ((a^x) - b) p == 0 then x
+        else is_solved $ x + 1
+    
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do 
+    describe "should be" $ do
+        it "simple method test" $ do 
+            (simple_method 3 13 17) `shouldBe` 4
+            (simple_method 3 14 17) `shouldBe` 9
+            (simple_method 3 15 17) `shouldBe` 6
+        it "pollard method test" $ do
+            pollard_rho 3 13 17 `shouldBe` simple_method 3 13 17
+            
+            
+    
+    
