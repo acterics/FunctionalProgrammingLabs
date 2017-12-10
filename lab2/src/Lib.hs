672a69ad301baff4193a0963924c80a70a8b3164
Oleg Lipskiy <lolego1601@gmail.com>
2017-12-10 20:58:54 +0200

    implement algorithm, not passed all tests

--- lab2/src/Lib.hs
+++ lab2/src/Lib.hs
@@ -17,27 +17,66 @@ module Lib(
 -- pollard_rho n = pollardStep 1 2 n 2 2
 
 
+pollard_rho :: Integer -> Integer -> Integer -> IO Integer
+pollard_rho a b p = pollard_step_base a b p 0 0 0 0 1 1 0
 
-pollard_rho a b p = pollard_step_base a b p 0 0 0 0 1 1 1
+-- new_step_value :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> [Integer]
+-- new_step_value a b p x ai bi  
+--     | (mod x 3) == 0 = [mod (x^2) p, mod (ai * 2) (p - 1), mod (bi * 2) (p - 1)]  
+--     | (mod x 3) == 1 = [mod (x * a) p, mod (ai + 1) (p - 1), mod bi (p - 1)]
+--     | (mod x 3) == 2 = [mod (x * b) p, mod ai (p - 1), mod(bi + 1) (p - 1)]
+f :: Integer -> Integer -> Integer -> Integer -> Integer
+f a b p x 
+    | (mod x 3) == 0 = mod (x^2) p
+    | (mod x 3) == 1 = mod (x * a) p
+    | (mod x 3) == 2 = mod (x * b) p
 
-new_step_value :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> [Integer]
-new_step_value a b p x ai bi  
-    | (mod x 3) == 0 = map f [x^2, ai * 2, bi * 2]  
-    | (mod x 3) == 1 = map f [x * a, ai + 1, bi]
-    | (mod x 3) == 2 = map f [x * b, ai, bi + 1] where
-        f x' = mod x' p
-    
+g :: Integer -> Integer -> Integer -> Integer
+g p x n
+    | (mod x 3) == 0 = mod (2 * n) (p - 1) 
+    | (mod x 3) == 1 = mod (n + 1) (p - 1)
+    | (mod x 3) == 2 = mod n (p - 1)
 
-pollard_step_base :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer
-pollard_step_base a b p ai a2i bi b2i xi x2i i = 
-    if xi == x2i then 
-        if bdif /= 0 then mod (quot adif bdif) p  
-        else -1
+h :: Integer -> Integer -> Integer -> Integer
+h p x n
+    | (mod x 3) == 0 = mod (2 * n) (p - 1) 
+    | (mod x 3) == 1 = mod n (p - 1)
+    | (mod x 3) == 2 = mod (n + 1) (p - 1) 
+
+-- g :: Integer -> Integer -> Integer -> Integer
+-- g p x n
+--     | (mod x 3) == 0 = mod (2 * n) p 
+--     | (mod x 3) == 1 = mod (n + 1) p
+--     | (mod x 3) == 2 = mod n p
+
+-- h :: Integer -> Integer -> Integer -> Integer
+-- h p x n
+--     | (mod x 3) == 0 = mod (2 * n) p 
+--     | (mod x 3) == 1 = mod n p
+--     | (mod x 3) == 2 = mod (n + 1) p 
+
+pollard_step_base :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> IO Integer
+pollard_step_base a b p ai a2i bi b2i xi x2i i = do
+    log
+    if xi == x2i && i /= 0 then if bdif /= 0 then return $ mod (quot adif bdif) (p - 1)  else return (-1)
     else new_step (i + 1) ival i2val where
         adif = a2i - ai
         bdif = bi - b2i
         new_step i' [xi', ai', bi'] [x2i', a2i', b2i'] = pollard_step_base a b p ai' a2i' bi' b2i' xi' x2i' i'
-        ival = new_step_value a b p xi ai bi
-        i2val = new_step_value a b p x2i a2i b2i
+        ival = [f a b p xi, g p xi ai, h p xi bi]
+        i2val = [f a b p get_x2i, g p get_x2i get_a2i, h p get_x2i get_b2i]
+        get_x2i = f a b p x2i
+        get_a2i = g p x2i a2i
+        get_b2i = h p x2i b2i
+        log = putStrLn $ foldl (++) "" [
+            show a, "^x = ", show b, " % ", show p,
+            " step: ", show i,
+            " ai:", show ai,
+            " bi:", show bi,
+            " xi:", show xi,
+            " a2i:", show a2i,
+            " b2i:", show b2i,
+            " x2i:", show x2i
+            ] 
 
     
--- lab2/test/Spec.hs
+++ lab2/test/Spec.hs
@@ -17,7 +17,9 @@ main = hspec $ do
             (simple_method 3 14 17) `shouldBe` 9
             (simple_method 3 15 17) `shouldBe` 6
         it "pollard method test" $ do
-            pollard_rho 3 13 17 `shouldBe` simple_method 3 13 17
+            (pollard_rho 2 5 1019) >>= \result -> result `shouldBe` simple_method 2 5 1019
+            (pollard_rho 2 991 1018) >>= \result -> result `shouldBe` simple_method 2 991 1018
+            (pollard_rho 3 15 17) >>= \result -> result `shouldBe` simple_method 3 15 17
             
             
     
